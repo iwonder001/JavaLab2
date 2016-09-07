5f7df525e6838d92aac1dff08a38bf432f0e618b
Kristen Carethers <kcarethers@gmail.com>
2016-09-07 17:29:27 -0400

    Add new project to git

---
+++ src/Adventure.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Adventure {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String name;
+		name = "";
+		
+		
+		Scanner input = new Scanner (System.in);	
+		
+		System.out.println("Welcome! What is your name? (enter your name):" + name );
+		input.nextLine();
+		
+	}
+
+}
