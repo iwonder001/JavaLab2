95a9cadbb94e2a590bd105696936baa5094b2d41
Kristen Carethers <kcarethers@gmail.com>
2016-09-08 20:39:15 -0400

    Added notes and explained code

--- src/Adventure.java
+++ src/Adventure.java
@@ -3,7 +3,6 @@ import java.util.Scanner;
 public class Adventure {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
 		String name;
 		name = "";
@@ -25,9 +24,11 @@ public class Adventure {
 		while (userContinue) {
 			System.out.println("Do you want to PARTY Freshman? (y/n)");
 			inputParty = input.nextLine();
+			//inputParty is holding the answer space for y or n.   
 
 			// testing if inputParty equals yes, then boolean party equals true.
-			// If not all ready declared boolean party = false at the beginning.
+			// If not already declared boolean party = false at the beginning.
+			//boolean party = false initialized at the beginning of the code.  Use if statement to run loop options if user said yes, so must make and initialize boolean party = true since I said it was false earlier. 
 			if (inputParty.equalsIgnoreCase("y")) {
 				party = true;
 			}
@@ -42,11 +43,14 @@ public class Adventure {
 					bikiniBash = true;
 				}
 
+				//if user says y they want to go to bikiniBash then they are given 3 clothing options which will be in a switch
+				//partyWear will hold bikini, naked or snowsuit answer
 				if (bikiniBash) {
 					System.out.println(
 							"Well choosen grasshopper!!! What are you going to wear to said party? (Bikini/Naked/Snowsuit) ");
 					partyWear = input.nextLine();
 
+					//why is "bikini", etc is quotation marks?
 					switch (partyWear) {
 					case "bikini":
 						System.out.println("Got drunk and you lost your bikini bottom. Run home!");
@@ -59,6 +63,7 @@ public class Adventure {
 						break;
 					}
 
+					//if user answers no to attending bikiniBash user will then be asked if they want to attend a Crossword party
 				} else {
 					System.out.println("Your parents would love it you spent your Friday at a Crossword Party.  What say you? (y/n)");
 					inputCrossword = input.nextLine();
@@ -73,6 +78,7 @@ public class Adventure {
 					}
 				} 
 
+				//if user says no to attending the party at the beginning of the loop then it will be false and get this answer.  This is the end of the while loop.
 				} else {
 				System.out.println("YOU ARE LAME!!!");
 				
@@ -82,6 +88,7 @@ public class Adventure {
 			System.out.println("Do you want to play again? (y/n)");
 			inputEnd=input.nextLine();
 			
+			//using variable values that were established in the first question where i asked the user their name. 
 			if(inputEnd.equalsIgnoreCase("n")) {
 				userContinue = false;
 				System.out.println("Thanks for playing " + name);
